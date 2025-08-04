a751fb082eb9eab16d5a42096eba0861f4afe9e2
dn011 <dkdkdodo123@gmail.com>
2025-08-04 22:22:12 +0900

    - 메인 화면 추가

--- app/src/main/java/com/example/qwer_test_kt/MainActivity.kt
+++ app/src/main/java/com/example/qwer_test_kt/MainActivity.kt
@@ -3,19 +3,43 @@ package com.example.qwer_test_kt
 import android.app.AlarmManager
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import com.example.qwer_test_kt.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val qwerText = "QWER"
+        val spannableString = SpannableString(qwerText)
+
+
+        spannableString.setSpan(ForegroundColorSpan(Color.parseColor("#FFFFFF")), 0, 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        spannableString.setSpan(ForegroundColorSpan(Color.parseColor("#FFC0CB")), 1, 2, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        spannableString.setSpan(ForegroundColorSpan(Color.parseColor("#00B0FF")), 2, 3, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        spannableString.setSpan(ForegroundColorSpan(Color.parseColor("#8BC34A")), 3, 4, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        binding.titleTextViewQWER.text = spannableString
+
+
+        binding.cardDiscord.setOnClickListener {
+           // val intent = Intent(this, PhotoWidgetActivity::class.java)
+            //startActivity(intent)
+        }
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
