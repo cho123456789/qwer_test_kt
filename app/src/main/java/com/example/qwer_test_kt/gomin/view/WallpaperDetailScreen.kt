145aa216e348ef74998c2b1665b995d7627ad9c9
cho <dn0868@igis.co.kr>
2025-09-24 08:40:17 +0900

    - 디테일 화면 버튼 하단 네비게이션바 가리는 현상 수정

--- app/src/main/java/com/example/qwer_test_kt/gomin/view/WallpaperDetailScreen.kt
+++ app/src/main/java/com/example/qwer_test_kt/gomin/view/WallpaperDetailScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
@@ -88,6 +89,7 @@ fun WallpaperDetailScreen(
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .navigationBarsPadding()
             .padding(bottom = 16.dp)
             .verticalScroll(scrollState),
         verticalArrangement = Arrangement.spacedBy(16.dp)
