adf8e5d16068208272965b0438461d7eb8bae0ed
cho <dkdkdodo123@gmail.com>
2025-12-03 08:45:56 +0900

    -  코드 갱신

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         applicationId = "com.example.qwer_test_kt"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
+        versionCode = 1 
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
